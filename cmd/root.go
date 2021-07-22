a821a1711f0eb051ec3dab0032820e5f461ba5f6
longkey1 <longkey1@gmail.com>
2021-07-23 01:37:40 +0900

    build: 0.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -28,7 +28,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Version: "0.0.2",
+	Version: "0.0.3",
 	Use:   "godl",
 	Short: "golang downloader",
 	// Uncomment the following line if your bare application
