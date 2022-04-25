cae3c5551c5e70f266d3af48f9cb490a28f3ed63
longkey1 <longkey1@gmail.com>
2022-04-25 22:57:04 +0900

    feat: replace goroots to path command

---
+++ cmd/path.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+// pathCmd represents the path command
+var pathCmd = &cobra.Command{
+	Use:   "path",
+	Short: "describe path",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if len(args) < 1 {
+			fmt.Println(cfg.GorootsDir)
+			return
+		}
+		target := args[0]
+
+		files, err := ioutil.ReadDir(cfg.GorootsDir)
+		cobra.CheckErr(err)
+
+		latestVer := ""
+		for _, file := range files {
+			if file.IsDir() == false {
+				continue
+			}
+			fmt.Println(target)
+			fmt.Println(file.Name())
+			if strings.Index(file.Name(), target) == 0 && latestVer < file.Name() {
+				latestVer = file.Name()
+			}
+		}
+		if len(latestVer) == 0 {
+			log.Fatalln("Not found matched version")
+		}
+
+		fmt.Println(filepath.Join(cfg.GorootsDir, latestVer))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(pathCmd)
+}
