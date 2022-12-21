81d5be7badb61faf5a49e8c9afb98ea557b8bdff
longkey1 <longkey1@gmail.com>
2022-12-21 11:08:03 +0900

    feat: add describe latest versions only option

--- cmd/list-remote.go
+++ cmd/list-remote.go
@@ -8,18 +8,18 @@ import (
 	"net/http"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-// listRemoteCmd represents the listRemote command
 var listRemoteCmd = &cobra.Command{
-	Use:   "list-remote",
+	Use:     "list-remote",
 	Aliases: []string{"ls-remote"},
-	Short: "downloadable version list",
+	Short:   "downloadable version list",
 	Run: func(cmd *cobra.Command, args []string) {
-		res, err := http.Get(cfg.GolangUrl+"/dl")
+		res, err := http.Get(cfg.GolangUrl + "/dl")
 		cobra.CheckErr(err)
 
 		defer func(Body io.ReadCloser) {
@@ -48,9 +48,45 @@ var listRemoteCmd = &cobra.Command{
 			}
 		})
 
-		versions := []string{}
+		var versions []string
 		for v := range versionMap {
 			versions = append(versions, v)
+		}
+
+		latest, err := cmd.Flags().GetBool("latest")
+		cobra.CheckErr(err)
+
+		if latest {
+			latestVersionMap := make(map[string]string)
+			for v := range versionMap {
+				fullVersion := v
+				if (strings.Count(fullVersion, ".")) == 1 {
+					fullVersion += ".0"
+				}
+				fullVersionSeparated := strings.Split(fullVersion, ".")
+				minorVersion := strings.Join([]string{fullVersionSeparated[0], fullVersionSeparated[1]}, ".")
+				_, has := latestVersionMap[minorVersion]
+				if has == false {
+					latestVersionMap[minorVersion] = fullVersion
+				} else {
+					for k, v := range latestVersionMap {
+						if k != minorVersion {
+							continue
+						}
+						latestVersionSeparated := strings.Split(v, ".")
+						fullVersionPatchNumber, _ := strconv.Atoi(fullVersionSeparated[2])
+						latestVersionPatchNumber, _ := strconv.Atoi(latestVersionSeparated[2])
+						if fullVersionPatchNumber > latestVersionPatchNumber {
+							latestVersionMap[minorVersion] = fullVersion
+						}
+					}
+				}
+			}
+			var latestVersions []string
+			for _, v := range latestVersionMap {
+				latestVersions = append(latestVersions, v)
+			}
+			versions = latestVersions
 		}
 
 		sort.Strings(versions)
@@ -62,4 +98,5 @@ var listRemoteCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(listRemoteCmd)
+	listRemoteCmd.Flags().Bool("latest", false, "filter latest version only")
 }
